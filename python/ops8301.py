8202d662a798f5297cc30fdce5e5e84e0ef26b07
jdavis199 <jadavis9697@gmail.com>
2023-06-26 21:11:14 -0400

    ops8301.py

---
+++ python/ops8301.py
@@ -0,0 +1,12 @@
+#John Davis 
+#OpsChallenge8
+#20230626
+#purpose: PYTHON
+
+Car = ["AlfoRomero","Bugatti","Chevy", "Dodge" , "Eagle" , "Ford" , "GMC" , "Hyundia", "Izuzu", "Jaguar"]
+print ( Car[3] ) 
+print ( Car [4] + "" + Car[9])
+Car[6] = 'Hummer'
+print (Car)
+
+#End
